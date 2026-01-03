2c5985c2a77dd47401aefde5a3d41e131e80f43f
Nancylak <nancylakra99@gmail.com>
2026-01-03 23:09:29 +0530

    Initial commit

--- SalessavvyBackend/src/main/java/com/example/Controller/UserController.java
+++ SalessavvyBackend/src/main/java/com/example/Controller/UserController.java
@@ -1,34 +1,26 @@
 package com.example.Controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Entity.User;
 import com.example.Service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/api/users")
 public class UserController {
-	private final UserService userService;
-	@Autowired
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
-	
-	@PostMapping("/register")
-	public ResponseEntity<?> registerUser(@RequestBody User user){
-		try {
-			User registeredUser = userService.registerUser(user);
-			return ResponseEntity.ok(Map.of("message", "User registered successfully", "user", registeredUser));
-		} catch(RuntimeException e) {
-			return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
-		}
-	}
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.registerUser(user);
+        return "redirect:/login"; // ✅ VERY IMPORTANT
+    }
 }
